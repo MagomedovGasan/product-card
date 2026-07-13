1e4019b3072077c309488db8b6e0b1f8810994f5
MagomedovGasan <gasan.005m@gmail.com>
2026-07-13 18:16:10 +0300

    madeChanges

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-//изменяем цвут всех карточек
+//изменяем цвет всех карточек
 const productCards = document.querySelectorAll('.card');
 const changeColorAllButton = document.querySelector('#Change-card-for-all-color-button');
 const greenColor = '#00FF00';
 const blueColor = '#0000FF'
 changeColorAllButton.addEventListener('click', () => {
   productCards.forEach((card) => card.style.backgroundColor = greenColor)
-})
+});
+
 
 //изменяем цвет первой карточки
+
 const firstProductCard = document.querySelector('.card');
 const changeColorFirstCardButton = document.querySelector('#Change-first-card-color-button');
 changeColorFirstCardButton.addEventListener('click', () => {
   firstProductCard.style.backgroundColor = blueColor
-})
+});
+
 
 //открываем гугл
-const openGoogleButton = document.querySelector('#open-google')
+
+const openGoogleButton = document.querySelector('#open-google');
 openGoogleButton.addEventListener('click',  () => {
   window.open('https://google.com')
-})
+});
+
 
 //выводим сообщение в консоль
+
 const outputLogButton = document.querySelector('#output-console-log');
 outputLogButton.addEventListener('click',() => outputConsoleLog ("ДЗ№6")
 
@@ -33,20 +39,18 @@ function outputConsoleLog(message){
 
 //добавил наведение на заголовок
 
-const element = document.querySelector('.tag');
+const title = document.querySelector('.productTitle');
 
-element.addEventListener('mouseover', () => {
-  console.log("Выбери свой продукт")
-  })
-
+title.addEventListener('mouseover', () => {
+  console.log(title.textContent)
+  });
 
 
   //кнопка меняющая цвет при нажатии
 
-  
-
-const toggleButton = document.querySelector('#btn-toggle');
+const toggleButton = document.querySelector('#btn-toggle'); 
+const heading = document.querySelector('.productTitle'); 
 
 toggleButton.addEventListener('click', () => {
-  toggleButton.classList.toggle('orange');
+    heading.classList.toggle('red-text'); 
 });
