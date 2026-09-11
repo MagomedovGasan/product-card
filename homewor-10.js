993e344a9666399af1dff30c22d66e66b72c3d9d
MagomedovGasan <gasan.005m@gmail.com>
2026-09-11 17:31:29 +0300

    made-changes

--- homewor-10.js
+++ homewor-10.js
@@ -1,18 +1,15 @@
 import {productsCatalog} from "./card.js"
 
-const userInput = prompt("Сколько карточек отобразить? От 1 до 5")
-console.log(userInput)
-
 function getQuantity () {
-const quantity = Number(userInput)
-console.log(Number.isNaN(quantity))
-if(isNaN(quantity) || quantity < 1 || quantity >= 5){
-  alert("Введите правильное число")
-  return null
+  const userInput = prompt("Сколько карточек отобразить? От 1 до 5")
+  const quantity = Number(userInput)
+  console.log(Number.isNaN(quantity))
+  if (isNaN(quantity) || quantity < 1 || quantity > 5){
+    alert("Введите правильное число")
+    return getQuantity ()
 }
   return quantity
 }
-
 
 // reduce
 
@@ -25,30 +22,17 @@ const nameDescription = productsCatalog.reduce((acc, product) => {
 }, [])
 console.log(nameDescription)
 
-//  Решение через map
-const a = productsCatalog.map(product => {
-  return{
-    name: product.name,
-    description: product.description
-  }
-})
-console.log(a)
-
-
-
- function renderCards (quantity){
-const productTemplate = document.querySelector("#product-template");
-const productList = document.querySelector(".product-list");
- productsCatalog.slice(0, quantity)
-
-productsCatalog.slice(0, quantity).forEach(product => {
-  const productClone = productTemplate.content.cloneNode(true);
-  productClone.querySelector("img").src = product.image
-  productClone.querySelector("img").alt = product.alt || product.name
-  productClone.querySelector(".card__label").textContent = product.label
-  productClone.querySelector(".card__title").textContent = product.name
-  productClone.querySelector(".card__description").textContent = product.description
-  productClone.querySelector(".price span").textContent = `${product.price} ${product.currency}`
+function renderCards (quantity) {
+  const productTemplate = document.querySelector("#product-template");
+  const productList = document.querySelector(".product-list");
+  productsCatalog.slice(0, quantity).forEach(product => {
+    const productClone = productTemplate.content.cloneNode(true);
+    productClone.querySelector("img").src = product.image
+    productClone.querySelector("img").alt = product.alt || product.name
+    productClone.querySelector(".card__label").textContent = product.label
+    productClone.querySelector(".card__title").textContent = product.name
+    productClone.querySelector(".card__description").textContent = product.description
+    productClone.querySelector(".price span").textContent = `${product.price} ${product.currency}`
 
   const componentList = productClone.querySelector(".component__list")
   product.compound.forEach(component => {
@@ -61,4 +45,3 @@ productsCatalog.slice(0, quantity).forEach(product => {
 }
 const currentQuantity = getQuantity(); 
 renderCards(currentQuantity);
-
