d100a7a41bdcf6bb0e4fdedf1908e158a7f354a1
MagomedovGasan <gasan.005m@gmail.com>
2026-09-16 19:12:13 +0300

    madeChanges3

--- homewor-10.js
+++ homewor-10.js
@@ -22,25 +22,31 @@ const nameDescription = productsCatalog.reduce((acc, product) => {
 }, [])
 console.log(nameDescription)
 
-function renderCards (quantity) {
+function renderCards(quantity) {
   const productTemplate = document.querySelector("#product-template");
   const productList = document.querySelector(".product-list");
+
   productsCatalog.slice(0, quantity).forEach(product => {
     const productClone = productTemplate.content.cloneNode(true);
-    productClone.querySelector("img").src = product.image
-    productClone.querySelector("img").alt = product.alt || product.name
-    productClone.querySelector(".card__label").textContent = product.label
-    productClone.querySelector(".card__title").textContent = product.name
-    productClone.querySelector(".card__description").textContent = product.description
-    productClone.querySelector(".price span").textContent = `${product.price} ${product.currency}`
-  const componentList = productClone.querySelector(".component__list")
-   product.compound.forEach(component => {
-    const li = document.createElement("li")
-    li.textContent = component
-    componentList.appendChild(li)
-  })
-   productList.appendChild(productClone);
-})
+
+    productClone.querySelector("img").src = product.image;
+    productClone.querySelector("img").alt = product.alt || product.name;
+    productClone.querySelector(".card__label").textContent = product.label;
+    productClone.querySelector(".card__title").textContent = product.name;
+    productClone.querySelector(".card__description").textContent = product.description;
+    productClone.querySelector(".price span").textContent = `${product.price} ${product.currency}`;
+
+    const componentList = productClone.querySelector(".component__list");
+
+    product.compound.forEach(component => {
+      const li = document.createElement("li");
+      li.textContent = component;
+      componentList.appendChild(li);
+    });
+
+    productList.appendChild(productClone);
+  });
 }
-const currentQuantity = getQuantity(); 
+
+const currentQuantity = getQuantity();
 renderCards(currentQuantity);
